746f80d78cd2b06d2b2fafdaa3f467b0691dba0f
Bastien Dejean <nihilhill@gmail.com>
2015-01-24 11:40:14 +0100

    Indent with tabs

--- backlight.c
+++ backlight.c
@@ -1,7 +1,7 @@
 #define _DEFAULT_SOURCE   // exposes u_short in glibc, needed by fts(3)
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/param.h>    // for MAXPATHLEN
+#include <sys/param.h>	  // for MAXPATHLEN
 #include <fts.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -9,7 +9,11 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef enum { CMD_CUR, CMD_MAX, CMD_UNDEF } CMD;
+typedef enum {
+	CMD_CUR,
+	CMD_MAX,
+	CMD_UNDEF
+} CMD;
 
 char backlight_interface[MAXPATHLEN];
 char actual_brightness_value[MAXPATHLEN];
@@ -17,143 +21,143 @@ char maximum_brightness_value[MAXPATHLEN];
 
 bool has_sysfs_backlight(void)
 {
-    bool found = false;
-    char *path_argv[] = { "/sys/class/backlight", NULL };
-    FTS *ftsp = fts_open(path_argv, FTS_LOGICAL | FTS_NOSTAT, NULL);
-    if (ftsp != NULL) {
-        fts_read(ftsp);
+	bool found = false;
+	char *path_argv[] = { "/sys/class/backlight", NULL };
+	FTS *ftsp = fts_open(path_argv, FTS_LOGICAL | FTS_NOSTAT, NULL);
+	if (ftsp != NULL) {
+		fts_read(ftsp);
 
-        struct stat statb;
-        FTSENT *cur = fts_children(ftsp, FTS_NAMEONLY);
-        while (!found && cur != NULL) {
-            snprintf(actual_brightness_value,
-                     sizeof (actual_brightness_value),
-                     "%s%s/actual_brightness",
-                     cur->fts_path,
-                     cur->fts_name);
-            if (stat(actual_brightness_value, &statb) == 0 &&
-                S_ISREG(statb.st_mode)) {
-                found = true;
-                snprintf(maximum_brightness_value,
-                         sizeof (maximum_brightness_value),
-                         "%s%s/max_brightness",
-                         cur->fts_path,
-                         cur->fts_name);
-                snprintf(backlight_interface,
-                         sizeof (backlight_interface),
-                         "%s%s/brightness",
-                         cur->fts_path,
-                         cur->fts_name);
-            }
-            cur = cur->fts_link;
-        }
-        fts_close(ftsp);
-    }
-    return found;
+		struct stat statb;
+		FTSENT *cur = fts_children(ftsp, FTS_NAMEONLY);
+		while (!found && cur != NULL) {
+			snprintf(actual_brightness_value,
+					 sizeof (actual_brightness_value),
+					 "%s%s/actual_brightness",
+					 cur->fts_path,
+					 cur->fts_name);
+			if (stat(actual_brightness_value, &statb) == 0 &&
+				S_ISREG(statb.st_mode)) {
+				found = true;
+				snprintf(maximum_brightness_value,
+						 sizeof (maximum_brightness_value),
+						 "%s%s/max_brightness",
+						 cur->fts_path,
+						 cur->fts_name);
+				snprintf(backlight_interface,
+						 sizeof (backlight_interface),
+						 "%s%s/brightness",
+						 cur->fts_path,
+						 cur->fts_name);
+			}
+			cur = cur->fts_link;
+		}
+		fts_close(ftsp);
+	}
+	return found;
 }
 
 CMD parse_command(char *msg)
 {
-    struct {
-        CMD id;
-        char *name;
-    } commands[] = {
-        { CMD_CUR, "current" },
-        { CMD_MAX, "maximum" },
-        { CMD_UNDEF, NULL }
-    }, *cur = commands, *match = NULL;
-    unsigned int nmatch = 0;
-    while (cur->name != NULL) {
-        if (strstr(cur->name, msg) == cur->name) {
-            nmatch++;
-            match = cur;
-        }
-        cur++;
-    }
-    return nmatch == 1 ? match->id : CMD_UNDEF;
+	struct {
+		CMD id;
+		char *name;
+	} commands[] = {
+		{ CMD_CUR, "current" },
+		{ CMD_MAX, "maximum" },
+		{ CMD_UNDEF, NULL }
+	}, *cur = commands, *match = NULL;
+	unsigned int nmatch = 0;
+	while (cur->name != NULL) {
+		if (strstr(cur->name, msg) == cur->name) {
+			nmatch++;
+			match = cur;
+		}
+		cur++;
+	}
+	return nmatch == 1 ? match->id : CMD_UNDEF;
 }
 
 bool parse_percent(int *v)
 {
-    unsigned int val;
-    FILE *src = fopen(maximum_brightness_value, "r");
-    if (src != NULL) {
-        int nv = fscanf(src, "%u", &val);
-        if (nv == 1)
-            *v = (*v / 100.0) * val;
-        else
-            return false;
-        fclose(src);
-    } else {
-        return false;
-    }
-    return true;
+	unsigned int val;
+	FILE *src = fopen(maximum_brightness_value, "r");
+	if (src != NULL) {
+		int nv = fscanf(src, "%u", &val);
+		if (nv == 1)
+			*v = (*v / 100.0) * val;
+		else
+			return false;
+		fclose(src);
+	} else {
+		return false;
+	}
+	return true;
 }
 
 int main(int argc, char *argv[])
 {
-    int exit_status = EXIT_SUCCESS;
+	int exit_status = EXIT_SUCCESS;
 
-    if (argc < 2) {
-        fprintf(stderr, "usage: backlight c[urrent]|m[aximum]|[+|-]VALUE[%%]\n");
-        exit_status = EXIT_FAILURE;
-    } else if (has_sysfs_backlight()) {
-        char *msg = argv[1];
-        if (isalpha(msg[0])) {
-            FILE *src = NULL;
-            switch (parse_command(msg)) {
-                case CMD_CUR:
-                    src = fopen(actual_brightness_value, "r");
-                    break;
-                case CMD_MAX:
-                    src = fopen(maximum_brightness_value, "r");
-                    break;
-                default:
-                    break;
-            }
-            if (src != NULL) {
-                unsigned int val;
-                int nv = fscanf(src, "%u", &val);
-                if (nv == 1)
-                    printf("%u\n", val);
-                else
-                    exit_status = EXIT_FAILURE;
-                fclose(src);
-            } else {
-                exit_status = EXIT_FAILURE;
-            }
-        } else {
-            FILE *src = fopen(actual_brightness_value, "r");
-            FILE *dst = fopen(backlight_interface, "w");
-            if (src != NULL && dst != NULL) {
-                char p = 0;
-                int next;
-                int nn = sscanf(msg, "%i%c", &next, &p);
-                if (nn == 1 || (nn == 2 && p == '%' && parse_percent(&next))) {
-                    if (msg[0] == '-' || msg[0] == '+') {
-                        unsigned int val;
-                        int nv = fscanf(src, "%u", &val);
-                        if (nv == 1) {
-                            fprintf(dst, "%u", val + next);
-                        } else {
-                            exit_status = EXIT_FAILURE;
-                        }
-                    } else {
-                        fprintf(dst, "%u", next);
-                    }
-                } else {
-                    exit_status = EXIT_FAILURE;
-                }
-            } else {
-                if (src != NULL)
-                    fclose(src);
-                if (dst != NULL)
-                    fclose(dst);
-            }
-        }
-    } else {
-        exit_status = EXIT_FAILURE;
-    }
+	if (argc < 2) {
+		fprintf(stderr, "usage: backlight c[urrent]|m[aximum]|[+|-]VALUE[%%]\n");
+		exit_status = EXIT_FAILURE;
+	} else if (has_sysfs_backlight()) {
+		char *msg = argv[1];
+		if (isalpha(msg[0])) {
+			FILE *src = NULL;
+			switch (parse_command(msg)) {
+				case CMD_CUR:
+					src = fopen(actual_brightness_value, "r");
+					break;
+				case CMD_MAX:
+					src = fopen(maximum_brightness_value, "r");
+					break;
+				default:
+					break;
+			}
+			if (src != NULL) {
+				unsigned int val;
+				int nv = fscanf(src, "%u", &val);
+				if (nv == 1)
+					printf("%u\n", val);
+				else
+					exit_status = EXIT_FAILURE;
+				fclose(src);
+			} else {
+				exit_status = EXIT_FAILURE;
+			}
+		} else {
+			FILE *src = fopen(actual_brightness_value, "r");
+			FILE *dst = fopen(backlight_interface, "w");
+			if (src != NULL && dst != NULL) {
+				char p = 0;
+				int next;
+				int nn = sscanf(msg, "%i%c", &next, &p);
+				if (nn == 1 || (nn == 2 && p == '%' && parse_percent(&next))) {
+					if (msg[0] == '-' || msg[0] == '+') {
+						unsigned int val;
+						int nv = fscanf(src, "%u", &val);
+						if (nv == 1) {
+							fprintf(dst, "%u", val + next);
+						} else {
+							exit_status = EXIT_FAILURE;
+						}
+					} else {
+						fprintf(dst, "%u", next);
+					}
+				} else {
+					exit_status = EXIT_FAILURE;
+				}
+			} else {
+				if (src != NULL)
+					fclose(src);
+				if (dst != NULL)
+					fclose(dst);
+			}
+		}
+	} else {
+		exit_status = EXIT_FAILURE;
+	}
 
-    return exit_status;
+	return exit_status;
 }
