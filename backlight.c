887b31c1201b10ae8cc65149128eae553c9f36ba
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2017-09-30 23:30:51 +0200

    Added support for setting backlight to absolute percentage
    Before backlight would interpret a percentage without (+/-) prefix as
    absolute brightness value, resulting in a very dark display in most
    cases.
    
    Now the trailing percent sign is properly interpreted and the given
    absolute percentage is set.

--- backlight.c
+++ backlight.c
@@ -143,7 +143,12 @@ int main(int argc, char *argv[])
 						} else {
 							exit_status = EXIT_FAILURE;
 						}
-					} else {
+					} else if (nn ==2 && p == '%') {
+						int val;
+						val = max/100.0f*next;
+						fprintf(dst, "%u", val > max ? max : val < 0 ? 0 : val);
+					}
+					else {
 						fprintf(dst, "%u", next > max ? max : next);
 					}
 				} else {
